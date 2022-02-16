cdf2684810a727390246b7df1873afea4af02de8
Edward Edberg <edward.edberg.halim@gmail.com>
2022-02-16 10:00:31 -0800

    add lazy loading

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -250,7 +250,7 @@ headerObserver.observe(header);
 // const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -265,6 +265,30 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  // replace drc with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: `200px`,
+});
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // const h1 = document.querySelector('h1');
 // const alertH1 = function (e) {
 //   alert(`addEventListener: Great! You're reading the heading.`);
